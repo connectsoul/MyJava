e628a579e84dc85650781b30107c0a49077f599d
Marat <univeweb@mail.ru>
2020-07-04 21:14:11 +0500

    Это наш первый коммит

---
+++ lesson 1/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(leftBorderWidth);
+
+//1)
+//console.log(typeof(4)));
+
+//2)
+//console.log("ww" + "err")
+
+//alert("Hello World!");
+
+let answer = confirm("Are you here?");
+
+console.log(answer);
